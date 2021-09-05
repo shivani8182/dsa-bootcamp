de4ddaea43eaf55093b831dfd98ab138f8d33072
shivani8182 <90090123+shivani8182@users.noreply.github.com>
2021-09-05 10:05:51 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, temp;
+    a=4;
+    b=5;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
